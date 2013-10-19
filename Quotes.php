166627ebe6eae32cb3f9194374d2676f3f4d610e
Chris Santala <csantala@gmail.com>
2013-10-18 20:25:07 -0400

    quotes class fixup + capitalized filename + fixed relative path for quotes.txt

--- Quotes.php
+++ Quotes.php
@@ -2,7 +2,7 @@
 
 class Quotes {
 	function rand_quote() {
-		$quotes = file_get_contents('quotes.txt');
+		$quotes = file_get_contents(realpath(dirname(__FILE__)).'/quotes.txt');
 		preg_match_all('/"([^"]+)"/', $quotes, $quotesArray);
 		$totalQuotes = count($quotesArray[0]) -1;
 		return $quotesArray[1][rand(0, $totalQuotes)];
